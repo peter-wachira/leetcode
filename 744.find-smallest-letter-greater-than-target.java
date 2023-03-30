638baf9937546c84d4104e057e1faafa3073a10f
peter-wachira <pwachira900@gmail.com>
2023-03-30 08:32:45 +0300

    solve nextGreatestLetter

--- 744.find-smallest-letter-greater-than-target.java
+++ 744.find-smallest-letter-greater-than-target.java
@@ -11,16 +11,20 @@ class Solution {
     public char nextGreatestLetter(char[] letters, char target) {
         int start = 0;
         int end = letters.length - 1;
+
         while (start <= end) {
+            // find the middle element
+            // int mid = (start + end) / 2; // might be possible that (start + end) exceeds
+            // the range of int in java
             int mid = start + (end - start) / 2;
-            if (target > letters[mid]) {
-                start = mid + 1;
-            } else {
+
+            if (target < letters[mid]) {
                 end = mid - 1;
+            } else {
+                start = mid + 1;
             }
-            answer = letters[start % letters.length];
         }
-        return answer;
+        return letters[start % letters.length];
     }
 }
 // @lc code=end
