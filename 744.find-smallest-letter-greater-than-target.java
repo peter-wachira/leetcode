8086dab6b64173998344bbe5abaa6e205d473d73
peter-wachira <pwachira900@gmail.com>
2023-03-30 08:12:22 +0300

    Create first commit

---
+++ 744.find-smallest-letter-greater-than-target.java
@@ -0,0 +1,23 @@
+/*
+ * @lc app=leetcode id=744 lang=java
+ *
+ * [744] Find Smallest Letter Greater Than Target
+ */
+
+// @lc code=start
+class Solution {
+    public char nextGreatestLetter(char[] letters, char target) {
+        int start = 0;
+        int end = letters.length - 1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (target > letters[mid]) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+            return letters[start % letters.length];
+        }
+    }
+}
+// @lc code=end
